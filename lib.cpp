e2af50e8901cc6a5f3cfb992c3f505e3b06a9391
KCaarlos <115487880+KCaarlos@users.noreply.github.com>
2022-12-12 21:30:05 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+#include "lib.h"
+using namespace std;
 
+float valore( float melvin[], int dim ){
+float num=0;
+      
+      for (int i=0; i<dim; i++){
+
+         if(melvin[i]>=num){
+            num=melvin[i];
+          }
+       }
+
+    return num;
+
+}

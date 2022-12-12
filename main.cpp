7fd457b47576c9801f6cdc9b160531b73b905e29
KCaarlos <115487880+KCaarlos@users.noreply.github.com>
2022-12-12 21:28:05 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    int a=0;
+    cin >> a;
+    float melvin[a], c;
+    
+    for(int i=0; i<a; i++){
+
+       cin >> melvin[i];
+    }
+
+    c=valore(melvin, a);
+    cout << c << endl;
+
+    return 0;
+
 }
